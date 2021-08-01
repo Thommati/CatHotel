918107bb62dee31f8831afb4bcb434c1b8649401
Matthew Thompson <thommati@outlook.com>
2021-08-01 17:30:40 -0600

    Set up empty folders; change tests to use Firefox

--- cathotel-ui/src/app/app.component.spec.ts
+++ cathotel-ui/src/app/app.component.spec.ts
@@ -20,16 +20,9 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'cathotel-ui'`, () => {
+  it(`should have as title 'Cat Hotel'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('cathotel-ui');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('cathotel-ui app is running!');
+    expect(app.title).toEqual('Cat Hotel');
   });
 });
--- cathotel-ui/src/app/app.component.ts
+++ cathotel-ui/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'cathotel-ui';
+  title = 'Cat Hotel';
 }
