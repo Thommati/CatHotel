4ef09a3737e4d3629bf91b3dfa7fb7ae93ba2218
Matthew Thompson <thommati@outlook.com>
2021-08-01 19:49:35 -0600

    Test that navbar is defined when app is rendered

--- cathotel-ui/src/app/app.component.spec.ts
+++ cathotel-ui/src/app/app.component.spec.ts
@@ -1,15 +1,19 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { MaterialModule } from './material/material.module';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
+        MaterialModule,
         RouterTestingModule
       ],
       declarations: [
-        AppComponent
+        AppComponent,
+        NavbarComponent,
       ],
     }).compileComponents();
   });
@@ -18,5 +22,12 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
+  });
+
+  it('should render the navbar', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    const navbar = fixture.nativeElement.querySelector('app-navbar');
+    expect(navbar).toBeDefined();
   });
 });
