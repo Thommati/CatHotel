254b8ce967a19b1f2f70420455f80e5154b5d78f
Matthew Thompson <thommati@outlook.com>
2021-08-01 18:07:41 -0600

    Add Angular Material

---
+++ cathotel-ui/src/app/material/material.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from "@angular/core";
+
+const materialComponents = [
+
+];
+
+@NgModule({
+  imports: [materialComponents],
+  exports: [materialComponents]
+})
+export class MaterialModule {}
