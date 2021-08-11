a36f5f0fbf25354496ab6c4142817be8d43a5f49
Matthew Thompson <thommati@outlook.com>
2021-08-11 13:05:30 -0600

    Add info-boxes to home page, rendered with ngFor

--- cathotel-ui/src/app/components/home/home.component.ts
+++ cathotel-ui/src/app/components/home/home.component.ts
@@ -3,18 +3,31 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  cardData = [
+    {
+      title: '24 Hour Service',
+      content:
+        'Like a regular hotel, The Cat Hotel is available 24-hours a day to dropoff and pickup your pet.',
+    },
+    {
+      title: 'On-site Veterinarian',
+      content: 'We have an on-site veterinarian hospital that provides 24-hour emergency care and can provide regular checkups and care.'
+    },
+    {
+      title: 'Individual Care',
+      content: 'Each animal recieves personalized and individual care depending on their needs - including special-needs animals.'
+    }
+  ];
 
   onBookedClicked(): void {
     // TODO: Implement button to navigate to booking page once it exists.
     console.log('onBookClicked.  Please implement me');
   }
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
