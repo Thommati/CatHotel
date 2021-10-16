ed1594d05d569a377fc726c0d71096c99ff5d0fd
Matthew Thompson <thommati@outlook.com>
2021-10-16 14:01:24 -0600

    Add AboutComponent to routing at /about

--- cathotel-ui/src/app/app-routing.module.ts
+++ cathotel-ui/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
 import { HomeComponent } from './components/home/home.component';
 import { LocationComponent } from './components/location/location.component';
 import { TourComponent } from './components/tour/tour/tour.component';
@@ -7,6 +8,7 @@ import { TourComponent } from './components/tour/tour/tour.component';
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'location', component: LocationComponent },
+  { path: 'about', component: AboutComponent },
   { path: 'tour', component: TourComponent },
 ];
 
