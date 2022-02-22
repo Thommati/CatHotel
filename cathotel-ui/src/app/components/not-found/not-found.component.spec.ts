798f3afe6a3c3d6b6559beeed85c013faa7deefc
Matthew Thompson <thommati@outlook.com>
2022-02-22 17:07:04 -0500

    Add comments to not-found tests

--- cathotel-ui/src/app/components/not-found/not-found.component.spec.ts
+++ cathotel-ui/src/app/components/not-found/not-found.component.spec.ts
@@ -5,17 +5,22 @@ import { NotFoundComponent } from './not-found.component';
 describe('NotFoundComponent', () => {
   let component: NotFoundComponent;
   let fixture: ComponentFixture<NotFoundComponent>;
+
+  // Used for mocking the injected Router
   let routerService;
   let routerServiceStub: Partial<Router>;
 
   beforeEach(async () => {
+    // Used for the injected Router
     routerServiceStub = {
       url: '/cows'
     };
 
     await TestBed.configureTestingModule({
       declarations: [ NotFoundComponent ],
+      // Used to inject the mockup of Router instead of a real Angular Router service.
       providers: [ { provide: Router, useValue: routerServiceStub } ] // mocking the Router dependency
+      // providers: [ { provide: Router, useValue: jasmine.createSpyObj('Router', ['url']) } ] // This one line could have done it all
     })
     .compileComponents();
   });
@@ -23,8 +28,11 @@ describe('NotFoundComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(NotFoundComponent);
     component = fixture.componentInstance;
+
+    // Used for the injected Router
     routerService = fixture.debugElement.injector.get(Router);
     // routerService = TestBed.inject(Router); /* alternate way to inject the service */
+
     fixture.detectChanges();
   });
 
